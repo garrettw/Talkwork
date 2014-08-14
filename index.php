9b7f9681dad8d5e29ecccd1506834143694c135d
Garrett W <garrettw87@gmail.com>
2014-08-14 16:37:33 +0000

    disabled old constructor. this code is not usable just yet.

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ require 'core/tw-init.php'; // Set up environment
 
 $tw = $dic->create('Talkwork\\App');
 
-// constructor params: input, router, storage.
+/*
 $tw = new \Talkwork\App($in,
     new \Talkwork\Router($in->route, DEF_MODULE, DEF_RESTYPE, DEF_RESID)
-    //new \Talkwork\TwDB(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_TBLPREFIX)
-);
+    new \Talkwork\TwDB(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_TBLPREFIX)
+); */
 echo $tw->out();
